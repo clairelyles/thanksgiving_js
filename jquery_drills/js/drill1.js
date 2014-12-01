87ace8fa6591236ffb55bc68fbc4a02cb2e7bfa5
clairelyles <claire.lyles@gmail.com>
2014-11-30 16:30:40 -0800

    Completed js drill 1 and 2

--- jquery_drills/js/drill1.js
+++ jquery_drills/js/drill1.js
@@ -1,17 +1,20 @@
 $(function(){
 
-	//When the ball is clicked it should fade out, 
-	//move to a new random location, and then fade back in. 
-	//The new location must be within the .stage div.
-	var ballMove = function(event) {
-		$(this).hide.fadeOut(5000);
-		// .css({
-		// 	left:(Math.random()*$(window).width()-20),
-  //           top:(Math.random()*$(window).height()-20),      
-  //        })
-	}
-   console.log('The dom is ready! (drill 1)');
-   $('.stage').on('click', '.shape ball', ballMove);
+// function sets all the jQuery methods to be applied on '.ball' once the element is clicked
+var ballMove = function(event) {
+	var randomLoc = Math.random();
+	// applies jQuery methods to '.ball', once the 'click' event listener is triggered
+	$(this)
+		.fadeOut('5000')
+		.animate({
+			top: randomLoc * 300,
+			right: randomLoc * 300,
+			}, 500)
+		.fadeIn('5000')
+		console.log('test');
+}
+
+// When the ball is clicked, pass the .ball css selector through the ballMove function
+$('.stage').on('click', '.ball', ballMove);
 
 });
-
--- jquery_drills/js/drill2.js
+++ jquery_drills/js/drill2.js
@@ -1,12 +1,53 @@
 $(function(){
 
+	$(document).on('keyup',function(event){
+		var up = 38;
+		var right = 39;
+		var down = 40;
+		var left = 37;
+		var letterS = 83;
+		var randomLoc = Math.random();
 
-    console.log('The dom is ready! (drill 2)');
-
-    $(document).on('keyup',function(event){
-        //output to console which key was pressed
-        console.log('The user pressed key: ',event.which);
-    });
-
+		if (event.keyCode === left) {
+			$('.stage')
+			.find('.shape')
+			.animate({
+				left: 0,
+			})
+			console.log('left worked!');
+		}
+		if (event.keyCode === up) {
+			$('.stage')
+			.find('.shape')
+			.animate({
+				top: 0,
+			})
+			console.log('up worked!');
+		}
+		if (event.keyCode === right) {
+			$('.stage')
+			.find('.shape')
+			.animate({
+				left: 800,
+			})
+			console.log('right worked!');
+		}
+		if (event.keyCode === down) {
+			$('.stage')
+			.find('.shape')
+			.animate({
+				top: 400,
+			})
+			console.log('down worked!');
+		}
+		if (event.keyCode === letterS) {
+			$('.stage')
+			// .find('.shape')
+			.effect('shake', {times: 2, direction: 'down', distance: 5}, 1000)
+			console.log('S worked!');
+		}
+		console.log('The user pressed key: ',event.which);
+	});
 });
+
 
