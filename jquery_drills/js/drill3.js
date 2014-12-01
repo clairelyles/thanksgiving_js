a39b04396ac1564f3f09869d894be07361ae2590
clairelyles <claire.lyles@gmail.com>
2014-11-30 19:43:57 -0800

    Completed drill #3

--- jquery_drills/js/drill3.js
+++ jquery_drills/js/drill3.js
@@ -1,8 +1,42 @@
 $(function(){
 
 
-    console.log('The dom is ready! (drill 3)');
-    
+
+
+
+
+
+
+
+
+
+	var $ball = $('.stage').find('.ball');
+	var $shape = $('.stage').find('.shape');
+
+	var removeBall = function(event) {
+		$(this)
+			.removeClass('ball')
+			.css('background-color', '#33cccc')
+	}
+
+	var addBall = function(event) {
+		$(this)
+			.addClass('ball')
+			.css('background-color', '#31698a')
+	}
+
+	$ball.on('mouseover', removeBall)
+	$shape.on('mouseout', addBall)
+
+// 	var addBall = function(event) {
+// 		$ball.addClass('ball')
+// 		$ball
+// 			.find('.shape')
+// 			.add
+// 		console.log('testing testing')
+// 	};
+
+// $ball.find('.shape').on('mouseout', addBall)
 
 });
 
